51279ae72d5b812c8f7556ec138d0ffe0885b6f1
Rishav Bharti <rishavbharti21@gmail.com>
2022-01-09 20:29:13 +0530

    Refactor

--- src/components/FormPageLayout/index.js
+++ src/components/FormPageLayout/index.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import classnames from 'classnames';
 
-import Tooltip from '@mui/material/Tooltip';
-
 import Button from 'src/components/Button';
 
 const MenuPageLayout = (props) => {
-  const { title, handleSave, containerClass } = props;
+  const { title, loading, handleSave, containerClass } = props;
 
   return (
-    <Tooltip
-      placement='top'
-      title='Save your changes before navigating away from this page otherwise they will be lost.'
-    >
-      <div>
-        <div className='p-6 pt-0 border-b border-labelText mb-8 flex justify-between'>
-          <h1 className='text-2xl font-bold'>{title}</h1>
-          <Button label='Save' type='submit' onClick={handleSave} />
-        </div>
-        <div className={classnames('lg:pl-6 lg:pr-32', containerClass)}>
-          {props.children}
-        </div>
+    <div>
+      <div className='p-6 pt-0 border-b border-labelText mb-8 flex justify-between'>
+        <h1 className='text-2xl font-bold'>{title}</h1>
+        <Button
+          label='Save'
+          type='submit'
+          onClick={handleSave}
+          loading={loading}
+        />
       </div>
-    </Tooltip>
+      <div className={classnames('lg:pl-6 lg:pr-32', containerClass)}>
+        {props.children}
+      </div>
+    </div>
   );
 };
 
