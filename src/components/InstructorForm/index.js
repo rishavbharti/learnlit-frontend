2235b29513ddb76a5052c20e1a06dec801bbf702
Rishav Bharti <rishavbharti21@gmail.com>
2022-01-10 16:46:39 +0530

    UI Improvements in instructors form

--- src/components/InstructorForm/index.js
+++ src/components/InstructorForm/index.js
@@ -30,69 +30,67 @@ const InstructorForm = () => {
   });
 
   const onSubmit = (data) => {
-    console.log('data ', data);
     dispatch(addInstructor(data.details));
   };
 
   const renderForm = () => {
     return (
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        // onSubmit={(e) => e.preventDefault()}
-        className='grid grid-cols-1 md:grid-cols-2 gap-5 items-center'
-      >
-        <Controller
-          name='details.name'
-          control={control}
-          rules={{
-            required: 'Name is required.',
-            minLength: {
-              value: 3,
-              message: 'Name must be at least 3 characters.',
-            },
-            maxLength: {
-              value: 80,
-              message: 'Name must be maximum 80 characters.',
-            },
-          }}
-          render={({ field, fieldState: { error } }) => (
-            <Input
-              label='Name'
-              type='text'
-              placeholder='Rishav Bharti'
-              error={!!error}
-              helperText={error ? error.message : null}
-              required
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name='details.headline'
-          control={control}
-          rules={{
-            required: 'Headline is required.',
-            minLength: {
-              value: 10,
-              message: 'Headline must be at least 10 characters.',
-            },
-            maxLength: {
-              value: 60,
-              message: 'Headline must be maximum 60 characters.',
-            },
-          }}
-          render={({ field, fieldState: { error } }) => (
-            <Input
-              label='Headline'
-              type='text'
-              placeholder='Software Engineer at learnlit or Architect'
-              error={!!error}
-              helperText={error ? error.message : null}
-              required
-              {...field}
-            />
-          )}
-        />
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 items-center'>
+          <Controller
+            name='details.name'
+            control={control}
+            rules={{
+              required: 'Name is required.',
+              minLength: {
+                value: 3,
+                message: 'Name must be at least 3 characters.',
+              },
+              maxLength: {
+                value: 80,
+                message: 'Name must be maximum 80 characters.',
+              },
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                label='Name'
+                type='text'
+                placeholder='Rishav Bharti'
+                error={!!error}
+                helperText={error ? error.message : null}
+                required
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name='details.headline'
+            control={control}
+            rules={{
+              required: 'Headline is required.',
+              minLength: {
+                value: 10,
+                message: 'Headline must be at least 10 characters.',
+              },
+              maxLength: {
+                value: 60,
+                message: 'Headline must be maximum 60 characters.',
+              },
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                label='Headline'
+                type='text'
+                placeholder='Software Engineer at learnlit or Architect'
+                error={!!error}
+                helperText={error ? error.message : null}
+                required
+                {...field}
+              />
+            )}
+          />
+        </div>
+
         <Controller
           name='details.bio'
           control={control}
@@ -118,94 +116,96 @@ const InstructorForm = () => {
             );
           }}
         />
-        <Controller
-          name='details.email'
-          control={control}
-          rules={{
-            required: 'Email is required.',
-          }}
-          render={({ field, fieldState: { error } }) => (
-            <Input
-              label='Email'
-              type='email'
-              placeholder='name@company.com'
-              error={!!error}
-              helperText={error ? error.message : null}
-              required
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name='details.social.website'
-          control={control}
-          render={({ field, fieldState: { error } }) => (
-            <Input
-              label='Website'
-              type='url'
-              placeholder='https://www.learnlit.in'
-              error={!!error}
-              helperText={error ? error.message : null}
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name='details.social.twitter'
-          control={control}
-          render={({ field, fieldState: { error } }) => (
-            <Input
-              label='Twitter'
-              type='url'
-              placeholder='https://twitter.com/learnlit_in'
-              error={!!error}
-              helperText={error ? error.message : null}
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name='details.social.youtube'
-          control={control}
-          render={({ field, fieldState: { error } }) => (
-            <Input
-              label='Youtube'
-              type='url'
-              placeholder='https://youtube.com/'
-              error={!!error}
-              helperText={error ? error.message : null}
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name='details.social.linkedin'
-          control={control}
-          render={({ field, fieldState: { error } }) => (
-            <Input
-              label='LinkedIn'
-              type='url'
-              placeholder='https://linkedin.com/'
-              error={!!error}
-              helperText={error ? error.message : null}
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name='details.social.facebook'
-          control={control}
-          render={({ field, fieldState: { error } }) => (
-            <Input
-              label='Facebook'
-              type='url'
-              placeholder='https://facebook.com/'
-              error={!!error}
-              helperText={error ? error.message : null}
-              {...field}
-            />
-          )}
-        />
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 items-center'>
+          <Controller
+            name='details.email'
+            control={control}
+            rules={{
+              required: 'Email is required.',
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                label='Email'
+                type='email'
+                placeholder='name@company.com'
+                error={!!error}
+                helperText={error ? error.message : null}
+                required
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name='details.social.website'
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                label='Website'
+                type='url'
+                placeholder='https://www.learnlit.in'
+                error={!!error}
+                helperText={error ? error.message : null}
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name='details.social.twitter'
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                label='Twitter'
+                type='url'
+                placeholder='https://twitter.com/learnlit_in'
+                error={!!error}
+                helperText={error ? error.message : null}
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name='details.social.youtube'
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                label='Youtube'
+                type='url'
+                placeholder='https://youtube.com/'
+                error={!!error}
+                helperText={error ? error.message : null}
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name='details.social.linkedin'
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                label='LinkedIn'
+                type='url'
+                placeholder='https://linkedin.com/'
+                error={!!error}
+                helperText={error ? error.message : null}
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name='details.social.facebook'
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <Input
+                label='Facebook'
+                type='url'
+                placeholder='https://facebook.com/'
+                error={!!error}
+                helperText={error ? error.message : null}
+                {...field}
+              />
+            )}
+          />
+        </div>
       </form>
     );
   };
