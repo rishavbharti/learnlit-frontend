be46377f7f990a69b64ba27595dbe8c7c121fa22
Rishav Bharti <rishavbharti21@gmail.com>
2022-04-20 23:06:29 +0530

    Removed unused code

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,7 +9,6 @@ import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
