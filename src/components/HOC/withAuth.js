cf58268f5b73fbe7bac3342063e97aa968e33779
Rishav Bharti <rishavbharti21@gmail.com>
2022-07-19 20:26:25 +0530

    Refactor `withAuth` HOC. Remove comments

--- src/components/HOC/withAuth.js
+++ src/components/HOC/withAuth.js
@@ -4,26 +4,20 @@ import { useSelector } from 'react-redux';
 
 import { isBrowser } from 'src/utils';
 
-// export default function WithAuth({ WrappedComponent, redirect = '/login' }) {
-//   const { isAuthenticated } = useSelector((state) => state.auth);
-//   const router = useRouter();
-
-//   if (isAuthenticated) return <WrappedComponent />;
-//   else router.push(redirect);
-// }
-
 export default function withAuth(WrappedComponent, location = '/login') {
   const ConditionalRedirectWrapper = (props) => {
     const { isAuthenticated } = useSelector((state) => state.auth);
     const router = useRouter();
 
-    if (isBrowser() && isAuthenticated) {
-      return <WrappedComponent {...props} />;
-    }
+    if (isBrowser()) {
+      if (isAuthenticated) {
+        return <WrappedComponent {...props} />;
+      }
 
-    if (isBrowser() && !isAuthenticated) {
-      router.push({ pathname: location });
-      return <></>;
+      if (!isAuthenticated) {
+        router.push({ pathname: location });
+        return <></>;
+      }
     }
 
     return null;
--- src/components/HOC/withoutAuth.js
+++ src/components/HOC/withoutAuth.js
@@ -4,18 +4,6 @@ import { useSelector } from 'react-redux';
 
 import { isBrowser } from 'src/utils';
 
-// export default function WithoutAuth(props) {
-//   const { redirect = '/' } = props;
-//   const { isAuthenticated } = useSelector((state) => state.auth);
-//   const router = useRouter();
-
-//   if (isAuthenticated) {
-//     router.push(redirect);
-//   }
-
-//   return props.children;
-// }
-
 export default function withoutAuth(WrappedComponent, location = '/') {
   const ConditionalRedirectWrapper = (props) => {
     const { isAuthenticated } = useSelector((state) => state.auth);
